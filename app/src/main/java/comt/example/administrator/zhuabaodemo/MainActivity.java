39b8f714a136502ee9d2f598072ba4fbda79ba2c
zhao6429598 <348088053@qq.com>
2016-11-23 01:18:13 +0800

    提交

---
+++ app/src/main/java/comt/example/administrator/zhuabaodemo/MainActivity.java
@@ -0,0 +1,14 @@
+package comt.example.administrator.zhuabaodemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
